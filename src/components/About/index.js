3df90b98eeae7286f8166c435bef878eb0b7fd7c
leelambicagudimetla <leelambicagudimetla@gmail.com>
2024-02-28 11:47:51 +0000

    practice

---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const About = () => (
+  <div className="home-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      alt="about"
+      className="home-img"
+    />
+    <h1 className="home-heading">About</h1>
+    <p className="para">I love to create! Iam a frontend web developer</p>
+  </div>
+)
+export default About
---
+++ src/components/Header/index.js
@@ -0,0 +1,35 @@
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const Header = () => (
+  <nav className="nav-header">
+    <div className="blog-container">
+      <div className="wave-div">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/wave-logo-img.png "
+          alt="wave"
+          height="50"
+        />
+        <h1 className="wave">Wave</h1>
+      </div>
+      <ul className="nav-menu">
+        <li>
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="/about">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="/contact">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+)
+export default Header
